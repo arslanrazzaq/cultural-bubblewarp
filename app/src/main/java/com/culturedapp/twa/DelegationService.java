778215a49b33ecf437b422d261234015c0a017d8
Muhammad Arslan Razzaq <arslanrazzaq47@gmail.com>
2022-03-19 10:51:18 +0500

    initial commit

---
+++ app/src/main/java/com/culturedapp/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package com.culturedapp.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
